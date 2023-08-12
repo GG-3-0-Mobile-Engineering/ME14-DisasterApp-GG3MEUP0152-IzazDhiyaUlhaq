4edf3f69cb9cc5d5238d458a1bc69490d7b938e6
Izaz Dhiyaulhaq <id.izaz123@gmail.com>
2023-08-12 23:13:10 +0700

    update DisasterViewModel test and add extensions-mockito

--- app/src/test/java/id/izazdhiya/disasterapp/DisasterViewModelTest.kt
+++ app/src/test/java/id/izazdhiya/disasterapp/DisasterViewModelTest.kt
@@ -58,64 +58,8 @@ class DisasterViewModelTest {
 
     @Test
     fun `getReports should return success`() = testDispatcher.runBlockingTest {
-        // Given
-        val mockReportData = ReportData(
-            reportType = "flood",
-            accessabilityFailure = 1,
-            airQuality = 1,
-            condition = 1,
-            evacuationArea = true,
-            evacuationNumber = 1,
-            fireDistance = 1.1,
-            fireLocation = FireLocation(108.5419283181, -6.7016487411),
-            fireRadius = FireRadius(108.5419283181, -6.7016487411),
-            floodDepth = 81,
-            impact = 1,
-            personLocation = PersonLocation(108.5419283181, -6.7016487411),
-            visibility = 1,
-            volcanicSigns = listOf(1, 2)
-        )
-        val mockTags = Tags(
-            districtId = 1,
-            localAreaId = "1",
-            instanceRegionCode = "ID-JB"
-        )
-        val mockGeometry = Geometry(
-            coordinates = listOf(108.5419283181, -6.7016487411),
-            type = "Point"
-        )
-        val mockProperties = Properties(
-            createdAt = "2023-08-12T13:08:00.620Z",
-            disasterType = "flood",
-            imageUrl = "https://images.petabencana.id/673c34fb-22c9-440f-a6b5-711959b09ddf.jpg",
-            partnerCode = 0,
-            partnerIcon = 0,
-            pkey = "322042",
-            reportData = mockReportData,
-            source = "grasp",
-            status = "confirmed",
-            tags = mockTags,
-            text = "Trainer Mulya Dewi_Muhamad Rizki Maulana",
-            title = 0,
-            url = "673c34fb-22c9-440f-a6b5-711959b09ddf",
-        )
-        val mockFeature = listOf(
-            Feature(
-                geometry = mockGeometry,
-                properties = mockProperties,
-                type = "Feature"
-            )
-        )
-        val mockResult = Result(
-            features = mockFeature,
-            type = "FeatureCollection"
-        )
-        val mockDisasterReports = DisasterReport(
-            result = mockResult,
-            statusCode = 200
-        )
 
-        `when`(repository.getReports("geojson")).thenReturn(mockDisasterReports)
+        `when`(repository.getReports("geojson")).thenReturn(createMockDisasterReports())
 
         // When
         val observer = mock(Observer::class.java) as Observer<Resource<DisasterReport>>
@@ -124,12 +68,27 @@ class DisasterViewModelTest {
         // Then
         verify(repository).getReports("geojson")
         verify(observer).onChanged(Resource.loading(null))
-        verify(observer).onChanged(Resource.success(mockDisasterReports))
+        verify(observer).onChanged(Resource.success(createMockDisasterReports()))
     }
 
     @Test
     fun `getReportsByProvince should return success`() = testDispatcher.runBlockingTest {
         // Given
+
+        val provinceId = "ID-JK"
+        `when`(repository.getReportsByProvince("geojson", provinceId)).thenReturn(createMockDisasterReports())
+
+        // When
+        val observer = mock(Observer::class.java) as Observer<Resource<DisasterReport>>
+        viewModel.getReportsByProvince(provinceId).observeForever(observer)
+
+        // Then
+        verify(repository).getReportsByProvince("geojson", provinceId)
+        verify(observer).onChanged(Resource.loading(null))
+        verify(observer).onChanged(Resource.success(createMockDisasterReports()))
+    }
+
+    private fun createMockDisasterReports(): DisasterReport {
         val mockReportData = ReportData(
             reportType = "flood",
             accessabilityFailure = 1,
@@ -181,22 +140,11 @@ class DisasterViewModelTest {
             features = mockFeature,
             type = "FeatureCollection"
         )
-        val mockDisasterReports = DisasterReport(
+
+        return DisasterReport(
             result = mockResult,
             statusCode = 200
         )
-
-        val provinceId = "ID-JK"
-        `when`(repository.getReportsByProvince("geojson", provinceId)).thenReturn(mockDisasterReports)
-
-        // When
-        val observer = mock(Observer::class.java) as Observer<Resource<DisasterReport>>
-        viewModel.getReportsByProvince(provinceId).observeForever(observer)
-
-        // Then
-        verify(repository).getReportsByProvince("geojson", provinceId)
-        verify(observer).onChanged(Resource.loading(null))
-        verify(observer).onChanged(Resource.success(mockDisasterReports))
     }
 
 }
