c2ab03954e06e91ce1ea5ea780399496d0ab8f83
Izaz Dhiyaulhaq <id.izaz123@gmail.com>
2023-08-14 16:47:04 +0700

    add unit test for SettingsViewModel

---
+++ app/src/test/java/id/izazdhiya/disasterapp/viewmodeltest/SettingsViewModelTest.kt
@@ -0,0 +1,67 @@
+package id.izazdhiya.disasterapp.viewmodeltest
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import id.izazdhiya.disasterapp.data.datastore.SettingsDataStore
+import id.izazdhiya.disasterapp.ui.viewmodel.SettingsViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.*
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+
+@ExperimentalCoroutinesApi
+class SettingsViewModelTest {
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var settingsDataStore: SettingsDataStore
+
+    private val testDispatcher = TestCoroutineDispatcher()
+    private val testScope = TestCoroutineScope(testDispatcher)
+
+    private lateinit var viewModel: SettingsViewModel
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.openMocks(this)
+        Dispatchers.setMain(testDispatcher)
+        viewModel = SettingsViewModel(settingsDataStore)
+    }
+
+    @After
+    fun cleanup() {
+        Dispatchers.resetMain()
+        testScope.cleanupTestCoroutines()
+    }
+
+    @Test
+    fun testGetTheme() {
+        val fakeThemeValue = true
+        `when`(settingsDataStore.getThemeSetting()).thenReturn(flowOf(fakeThemeValue))
+
+        val observer = Observer<Boolean> {}
+
+        try {
+            viewModel.getTheme().observeForever(observer)
+        } finally {
+            viewModel.getTheme().removeObserver(observer)
+        }
+    }
+
+    @Test
+    fun testSaveTheme() = testScope.runBlockingTest {
+        val fakeThemeValue = true
+
+        viewModel.saveTheme(fakeThemeValue)
+
+    }
+}
